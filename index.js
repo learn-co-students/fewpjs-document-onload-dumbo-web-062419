febcd6d3256c30ec1b6a829947257c9db84ce37a
github username <github email address>
2019-08-07 08:20:24 -0400

    The browser shows what it needs to show but the tests are not passing

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function (){
+    console.log("The DOM has loaded");
+    targetedtext = document.getElementById("text")
+    targetedtext.innerHTML ="<p>This is really cool!</p>"
+})
